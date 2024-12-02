dec001047b2cb2f36a3117a7482e9e901a3265eb
太卜『符玄』 <fuxuan001@foxmail.com>
2024-12-02 23:18:16 +0800

    Delete Fuxuan directory and add new

--- Fuxuan/core/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .TimeDivination import TimeDivination
-from .interpretation import Interpretation
-from .NumDivination import NumDivination , Num
-from . import excformat
-
-__all__ = ['TimeDivination', 'excformat', 'Interpretation', 'NumDivination', 'Num']
