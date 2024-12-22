fafc8e48feb93d0a5087220075a7aaa383a4bc06
太卜『符玄』 <fuxuan001@foxmail.com>
2024-12-22 14:54:36 +0800

    Delete Fuxuan directory and add new

--- Fuxuan/model/__init__.py
+++
@@ -1,7 +0,0 @@
-""" 数据模型 """
-
-from ._result import DivinationResult, DivinationComment
-from ._type import hexagramType, hexagramsType
-from ._enums import DivType
-
-__all__ = ['DivinationResult', 'hexagramType', 'hexagramsType', 'DivType', 'DivinationComment']
--- Fuxuan/model/_enums.py
+++
@@ -1,9 +0,0 @@
-
-from enum import StrEnum
-
-class DivType(StrEnum):
-    """占卜类型"""
-    LUCK = "luck" # 吉凶
-    ITEM = "item" # 寻物
-    NONE = "none" # 无占卜
-    ...
