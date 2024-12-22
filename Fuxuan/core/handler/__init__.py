6cae0d688f1723c92ca5a76b31ceb946d6d2e1c5
太卜『符玄』 <fuxuan001@foxmail.com>
2024-12-22 19:40:15 +0800

    Delete Fuxuan directory

--- Fuxuan/core/handler/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-占卜和起卦等处理器模块
-"""
-
-from .DivHandlers import DIVINATION_HANDLER
-
-__all__ = [
-    'DIVINATION_HANDLER'
-]
--- Fuxuan/model/__init__.py
+++
@@ -1,7 +0,0 @@
-""" 数据模型 """
-
-from ._result import DivinationResult, DivinationComment
-from ._type import hexagramType, hexagramsType
-from ._enums import DivinationType , IttrType
-
-__all__ = ['DivinationResult', 'hexagramType', 'hexagramsType', 'DivinationType', 'IttrType', 'DivinationComment']
